3de6704d915c0685885752bfc50d737efd42dc40
Ruben Castaño <ruben.castano@btp.es>
2024-07-30 11:19:32 +0200

    Se crea el menu lateral

--- crm/src/app/app.component.ts
+++ crm/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [RouterOutlet, MenuComponent]
 })
 export class AppComponent {
   title = 'crm';
---
+++ crm/src/app/app.modules.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { MenuComponent } from './components/menu/menu.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MenuComponent
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule {
+  constructor() {
+  }
+}
