9709bf67dc05d03b58b88e980d7026915d588270
Ruben Castaño <ruben.castano@btp.es>
2024-07-31 10:36:32 +0200

    Se hace el nabvar responsive

--- crm/src/app/components/menu/menu.component.ts
+++ crm/src/app/components/menu/menu.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule]
 })
 export class MenuComponent implements OnInit {
+
+  menuOpen = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.checkWindowWidth();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkWindowWidth();
+  }
+
+  checkWindowWidth() {
+    if (window.innerWidth >= 768) {
+      this.menuOpen = true;
+    } else {
+      this.menuOpen = false;
+    }
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
